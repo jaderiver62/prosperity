8353de140f05b25e0a4bdb68a4754fc6ba2d3a76
Nina Jade <jaderiver64@gmail.com>
2021-04-19 16:06:13 -0500

    service-worker updates

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 const APP_PREFIX = "ProsperityApp-";
 const VERSION = "v01";
 const CACHE_NAME = APP_PREFIX + VERSION;
@@ -13,3 +15,36 @@ const FILES_TO_CACHE = [
     "./icons/icon-144x144.png",
     "./icons/icon-512x512.png"
 ];
+self.addEventListener("fetch", event => {
+            console.log("Fetch Request: " + event.request.url);
+            if (event.request.url.includes("/api/")) {
+                event.respondWith(
+                    caches.open(DATA_CACHE_NAME)
+                    .then(cache => {
+                        return fetch(event.request)
+                            .then(response => {
+                                if (response.status === 200) {
+                                    cache.put(event.request.url, response.clone());
+                                }
+                                return response;
+                            })
+                            .catch(err => cache.match(event.request));
+                    })
+                    .catch(err => console.log(err))
+                );
+                return;
+            }
+            event.respondWith(
+                fetch(event.request)
+                .catch(() => {
+                    return caches.match(event.request)
+                        .then(response => {
+                            if (response) {
+                                return response;
+                            } else if (event.request.headers.get("accept").includes("text/html")) {
+                                return caches.match("/index.html");
+                            }
+                        })
+                })
+            );
+        }
